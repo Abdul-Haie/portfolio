d46df9e3762dcdf0e6d185886bbbaf990c7b3217
Abdul-Haie <abdul.haie.kahloon@gmail.com>
2024-07-15 03:17:00 +0500

    added links in the navbar social media icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,28 @@ import React from 'react';
 import logo from "../assets/ABKLogo.png";
 import { FaLinkedin, FaGithub, FaTwitter, FaInstagram } from 'react-icons/fa';
 
-
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-      <img className='mx-4 w-16' src={logo} alt="logo" />
+        <img className='mx-4 w-16' src={logo} alt="logo" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-      <FaLinkedin />
-      <FaGithub />
-      <FaTwitter />
-      <FaInstagram />
-    </div>
+        <a href="https://www.linkedin.com/in/abdul-haie-b9538b215/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/Abdul-Haie" target="_blank" rel="noopener noreferrer">
+          <FaGithub />
+        </a>
+        <a href="https://twitter.com/your-profile" target="_blank" rel="noopener noreferrer">
+          <FaTwitter />
+        </a>
+        <a href="https://www.instagram.com/abdulhaie_kahloon/" target="_blank" rel="noopener noreferrer">
+          <FaInstagram />
+        </a>
+      </div>
     </nav>
   )
 }
 
 export default Navbar;
-
-
